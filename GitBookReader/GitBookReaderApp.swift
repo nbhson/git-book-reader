aeb2d2b67b54f023d3f01754b91b8f886777ce88
Sơn Nguyễn <nbhson43@gmail.com>
2026-02-20 10:55:05 +0700

    Initial Commit

---
+++ GitBookReader/GitBookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBookReaderApp.swift
+//  GitBookReader
+//
+//  Created by Sơn Nguyễn on 20/2/26.
+//
+
+import SwiftUI
+
+@main
+struct GitBookReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
